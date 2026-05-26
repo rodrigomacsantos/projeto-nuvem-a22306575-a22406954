f752e159aec40dc169ee14688870be1f3433b13b
Lourencol18 <lourenco20041@gmail.com>
2026-05-26 23:09:17 +0100

    adicionar vpc subredes etc terraoform

--- infrastructure/terraform/environments/dev/backend.tf
+++ infrastructure/terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "cloudms-tf-state-ue-east-1"
+    bucket         = "cloudms-tf-state-us-east-1"
     key            = "envs/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "cloudms-tf-locks"
